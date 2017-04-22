844576e4dd0f5b9ac7322c65044b60068bd53ba8
david <david.l.castilla@gmail.com>
2017-04-22 17:34:12 -0400

    create function to load audio

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("hello from app.js!");
+const input = document.getElementById('input');
+const submit = document.getElementById('submit');
+
+const fetchAudio = (url) => {
+	return new Promise((resolve, reject) => {
+		var audio = new Audio(url);
+	  audio.addEventListener('canplaythrough', function() { 
+	  	resolve(audio);
+		}, false);
+	});
+
+}
+
+const init = () => {
+	fetchAudio(input.value)
+		.then((val) => {console.log("audio loaded", val)})
+}
+
+submit.addEventListener('click', evt => {init()});
